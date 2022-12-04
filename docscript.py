2b72f8cbe480ea8c4e679fe6778c7a0e9ae97266
Angel Cascarino <angel.cascarino@googlemail.com>
2022-12-04 22:03:49 +0000

    Update script to generate the days as well

--- docscript.py
+++ docscript.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
 here = Path(".")
+for day in range(1, 26):
+    newdir = here / f"day{day:02}"
+    newdir.mkdir(parents=True, exist_ok=True)
 for child in here.rglob("**/day*"):
     newpydir = child / "py"
     newcppdir = child / "cpp"
