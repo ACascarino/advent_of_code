1673bf6d5804ad262cacd3da4465ce1beb5300ed
Angel Cascarino <angel.cascarino@googlemail.com>
2022-12-06 23:11:11 +0000

    Refactor for neatness

--- day06/py/part1.py
+++ day06/py/part1.py
@@ -1,9 +1,17 @@
+def all_unique(sequence):
+    return len(set(sequence)) == len(sequence)
+
+
+def find_unique_substring_of_length_x(sequence, length):
+    for position in range(len(sequence)):
+        substring = sequence[position : position + length]
+        if all_unique(substring):
+            return position
+
+
 if __name__ == "__main__":
     with open("input.txt", "r") as file:
         puzzle_input = file.read()
 
-    for position in range(len(puzzle_input)):
-        sequence = puzzle_input[position : position + 4]
-        if len(set(sequence)) == len(sequence):
-            print(position + 4)
-            break
+    substring_start = find_unique_substring_of_length_x(puzzle_input, 4)
+    print(substring_start + 4)
--- day06/py/part2.py
+++ day06/py/part2.py
@@ -1,9 +1,8 @@
+import part1
+
 if __name__ == "__main__":
     with open("input.txt", "r") as file:
         puzzle_input = file.read()
 
-    for position in range(len(puzzle_input)):
-        sequence = puzzle_input[position : position + 14]
-        if len(set(sequence)) == len(sequence):
-            print(position + 14)
-            break
+    substring_start = part1.find_unique_substring_of_length_x(puzzle_input, 14)
+    print(substring_start + 14)
