15f8576b9fffefe0ac6f6a6b4011c815ac0e3fb1
Angel Cascarino <angel.cascarino@googlemail.com>
2024-12-03 22:28:16 +0000

    Remove empty files 2021

--- 2021/docscript.py
+++ 2021/docscript.py
@@ -1,7 +1,8 @@
 import advent_of_code_utils as aoc_utils
-import os
+import shutil
 
 folder_2021 = aoc_utils.here().parent
 for subfolder in folder_2021.glob("day*"):
-    if (subfolder / "c").exists() and (subfolder / "c").glob("*.py") is not None:
-        os.rmdir(subfolder / "c")
+    if (subfolder / "c").exists():
+        if not sorted((subfolder / "c").glob("*.py")) == []:
+            shutil.rmtree(subfolder / "c")
