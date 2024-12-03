470448f00bb6f5df93ab4c0f894edaf14d86a335
Angel Cascarino <angel.cascarino@googlemail.com>
2024-12-03 22:41:32 +0000

    Have 2024 docscript reflect reality

--- 2024/docscript.py
+++ 2024/docscript.py
@@ -6,9 +6,7 @@ for day in range(1, 26):
     newdir.mkdir(parents=True, exist_ok=True)
 for child in here.rglob("**/day*"):
     newpydir = child / "py"
-#    newcppdir = child / "c"
     newpydir.mkdir(parents=True, exist_ok=True)
-#    newcppdir.mkdir(parents=True, exist_ok=True)
     with (child / "input.txt").open("a") as file:
         file.write("")
     with (child / "test.txt").open("a") as file:
@@ -17,9 +15,3 @@ for child in here.rglob("**/day*"):
         file.write("")
     with (newpydir / "part2.py").open("a") as file:
         file.write("")
-#    with (newcppdir / "part1.c").open("a") as file:
-#        file.write("")
-#    with (newcppdir / "part2.c").open("a") as file:
-#        file.write("")
-#    with (child / "CMakeLists.txt").open("a") as file:
-#        file.write("")
