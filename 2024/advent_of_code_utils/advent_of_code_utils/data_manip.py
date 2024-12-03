59ca4205429d0afbfd10bb9987f48f49fcf26a2f
Angel Cascarino <angel.cascarino@googlemail.com>
2024-12-03 21:52:57 +0000

    Move to outer folder

--- 2024/advent_of_code_utils/advent_of_code_utils/data_manip.py
+++
@@ -1,5 +0,0 @@
-def input_split_int(puzzle_input:str) -> list[list[int]]:
-    """
-    "1 2 3 4\n5 6 7 8\n9 10 11 12" -> [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
-    """
-    return [[int(x) for x in line.strip().split()] for line in puzzle_input.splitlines()]
