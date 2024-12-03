e59d5699749ac24107900b7c3ad769c3e58d999d
Angel Cascarino <angel.cascarino@googlemail.com>
2024-12-03 22:33:57 +0000

    Remove more historical empty files

--- 2021/docscript.py
+++
@@ -1,7 +0,0 @@
-import advent_of_code_utils as aoc_utils
-import shutil
-
-folder_2021 = aoc_utils.here().parent
-for subfolder in folder_2021.glob("day*"):
-    if not (subfolder / "c").exists() or (subfolder / "py").exists():
-        shutil.rmtree(subfolder)
