4e7cd8f3273ef4d0d89994703fedbd2818e2e0f5
Angel Cascarino <angelcascarino@xmos.com>
2024-12-04 14:46:15 +0000

    Solve D3P2

--- 2024/day03/py/part2.py
+++ 2024/day03/py/part2.py
@@ -3,11 +3,21 @@ import re
 
 
 def app(puzzle_input: str) -> int:
-    search_string = r"(mul\((%d*?),(%d*?)\))|(do\(\))|(don't\(\))"
-    found = re.findall(search_string, puzzle_input)
-    aoc_utils.say([found])
+    search_string = r"(?:mul\((\d*?),(\d*?)\))|(do\(\))|(don't\(\))"
+    solution = 0
+    mul = True
+    for found in re.findall(search_string, puzzle_input):
+        match found:
+            case ("", "", "", "don't()"):
+                mul = False
+            case ("", "", "do()", ""):
+                mul = True
+            case (a, b, "", ""):
+                if mul:
+                    solution += int(a) * int(b)
+    return solution
 
 
 if __name__ == "__main__":
-    solution = app(aoc_utils.get_test())
+    solution = app(aoc_utils.get_input())
     aoc_utils.say(solution)
